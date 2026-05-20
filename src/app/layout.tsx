dc449af18931a7f69196b8cb40dfd85208d508b0
P-Wall86 <sisterwall@gmail.com>
2026-05-20 16:55:15 -0300

    Fonts Test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Newsreader } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const newsreader = Newsreader({
+  variable: "--font-newsreader",
   subsets: ["latin"],
 });
 
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${inter.variable} ${newsreader.variable}`}>
       <body>{children}</body>
     </html>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+    return (<header>
+        <h1>Handcrafted Haven</h1>
+        <nav>
+            <a>Shop</a>
+            <a>My Profile</a>
+            <button>Basket</button>
+        </nav>
+    </header>
+    );
+}
