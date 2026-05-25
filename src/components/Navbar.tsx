f6710bc5a8864d4872fcaa3853f5b0668e55fc8c
moroninavarro <moronialanis23@gmail.com>
2026-05-25 16:32:01 -0700

    updating navigation/ and create the basket and profile folders

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import style from "./Navbar.module.css";
+import Link from "next/link";
+
 
 export default function Navbar() {
-    return (<header>
+    return (
+    <header className={style.header}>
         <h1 className={style.h1}>Handcrafted Haven</h1>
-        <nav>
-            <a>Shop</a>
-            <a>My Profile</a>
-            <button>Basket</button>
+        <nav className={style.nav}>
+            <Link href="/">Shop</Link>
+            <Link href="/profile">My Profile</Link>
+            
+            <Link href="/basket" className={style.button}>
+            Basket 
+            </Link>
         </nav>
     </header>
     );
